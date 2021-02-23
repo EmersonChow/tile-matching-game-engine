9abcf57f7e518ed017fc97eaa09baab7bb41d74d
kati-tran <katit1@uci.edu>
2021-02-22 21:05:11 -0800

    Randomized tile colors

---
+++ src/main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import javax.swing.*;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated constructor stub
+		@SuppressWarnings("resource")
+		Scanner cal = new Scanner(System.in);  // Create a Scanner object
+	    System.out.println("Choose a game: memory or bejeweled");
+	    
+	    if(cal.next().equals("memory")) {
+	    	SwingUtilities.invokeLater(() -> new memory(4, 4));
+	    }
+	}
+
+}
